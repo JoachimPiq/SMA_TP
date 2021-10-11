5802b2cabac567d6434d1b8d384c96fd5a3b26e1
Joachim Piqueret <joachim.pourriel@gmail.com>
2021-10-11 13:56:43 +0200

    push fin tp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Environement env = new Environement(4);
+        System.out.println(env);
+        Random rand = new Random();
+        while (!env.succes()){
+            char nextAgent  = ("A" + rand.nextInt(3)).charAt(0);
+            System.out.println(nextAgent);
+            Agent agent = env.getAgent(nextAgent);
+            agent.action();
+            System.out.println(env);
+        }
+
+
     }
 }
